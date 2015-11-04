c6d4db34bcca5aa6cc86a4e6f670a858c2f0b6bc
David Watkins <djrival7@gmail.com>
2015-11-04 13:36:38 -0500

    Cleaned up git directory

--- 1-Initial Host-Worker Code/test.py
+++
@@ -1,8 +0,0 @@
-def f(nums):
-    return [num+1 for num in nums]
-
-if __name__ == "__main__":
-    data = [1,2,3,4,5]
-    result = ",".join([str(item) for item in f(data)])
-    with open("new_file", "w") as f:
-        f.write(result)
