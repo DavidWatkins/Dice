34586a39bbe449392a730dcbcf3e85dd2b70941c
David Watkins <davidw@tkins.me>
2015-11-30 23:46:51 -0500

    Merged Emily's changes to master

--- Compiler/analyzer.ml
+++ Compiler/analyzer.ml
@@ -5,6 +5,7 @@ open Utils
 open Filepath
 
 module StringMap = Map.Make (String)
+
 module SS = Set.Make(
     struct
         let compare = Pervasives.compare
@@ -17,6 +18,11 @@ module TM = Map.Make(
         type t = op
     end )
 
+module Type_to_String = Map.Make(
+    struct
+        let compare = Pervasives.compare
+        type t = datatype
+    end )
 
 type class_map = {
 		field_map       : Ast.field StringMap.t;
@@ -28,7 +34,7 @@ type class_map = {
 type env = {
 		env_class_map : class_map;
 		env_name      : string;
-		env_locals    : Ast.datatype StringMap.t;
+		env_locals    : datatype StringMap.t;
 		env_parameters: Ast.formal StringMap.t;
 		env_returnType: datatype;
 		env_callStack : stmt list;
@@ -116,7 +122,12 @@ let get_arithmetic_binop_type se1 se2 op = function
         | _ -> raise (Exceptions.InvalidBinopExpression "Arithmetic operators don't support these types")
 
 
-let rec get_ID_type env s = Datatype(Int_t)
+let print_map_bindings map = 
+let ts = List.fold_left (fun map (key, value) -> Type_to_String.add key value map) Type_to_String.empty [(Datatype(Int_t), "INT"); (Datatype(Float_t), "FLOAT"); (Datatype(Char_t), "CHAR"); (Datatype(Bool_t), "BOOL"); (Datatype(Void_t), "VOID"); (Datatype(Null_t), "NULL")] in 
+StringMap.iter (fun k v -> print_endline (k ^ " " ^ (Type_to_String.find v ts))) map
+
+
+let rec get_ID_type env s = StringMap.find s env.env_locals
 
 and check_array_primitive env el = SInt_Lit(0, Datatype(Int_t))
 
@@ -267,7 +278,9 @@ let rec convert_stmt_list_to_sstmt_list (env:env) stmt_list =
 		|  	Break 					-> SBreak, env (* Need to check if in right context *)
 		|   Continue 				-> SContinue, env (* Need to check if in right context *)
 
-		|   Local(d, s, e) 			-> 	let se, env = expr_to_sexpr env e in
+		|   Local(d, s, e) 			-> 	if StringMap.mem s env.env_locals then raise (Exceptions.DuplicateLocal s)
+                                        else
+                                        let se, env = expr_to_sexpr env e in
 										let t = get_type_from_sexpr se in
 										if t = Datatype(Void_t) || t = d (* AND s not in env.locals *) 
 										then 
