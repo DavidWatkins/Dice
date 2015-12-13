c71bfa88710ef0a7c39f98fb4ece382a6dbb877c
David Watkins <davidw@tkins.me>
2015-12-12 19:04:56 -0500

    ArrayCreate doesn't work, added code for array deref

--- Compiler/codegen.ml
+++ Compiler/codegen.ml
@@ -262,7 +262,7 @@ and codegen_assign lhs rhs d llbuilder =
 	let lhs = match lhs with
 	| 	Sast.SId(id, d) -> codegen_id false false id d llbuilder
 	|  	SObjAccess(e1, e2, d) -> codegen_obj_access false e1 e2 d llbuilder
-	| 	SArrayAccess(se, sel, d) -> codegen_array_access se sel d llbuilder
+	| 	SArrayAccess(se, sel, d) -> codegen_array_access true se sel d llbuilder
 	| _ -> raise Exceptions.AssignLHSMustBeAssignable
 	in
 	(* Codegen the rhs. *)
@@ -326,9 +326,12 @@ and codegen_string_lit s llbuilder =
 	else if s = "false" then build_global_stringptr "false" "" llbuilder
 	else build_global_stringptr s "" llbuilder
 
-and codegen_array_access e el d llbuilder = 
+and codegen_array_access isAssign e el d llbuilder = 
 	let x = List.map (codegen_sexpr llbuilder) el in 
-	build_gep (codegen_sexpr llbuilder e) (Array.of_list x) "" llbuilder
+	let _val = build_gep (codegen_sexpr llbuilder e) (Array.of_list x) "" llbuilder in
+	if isAssign 
+		then build_load _val "" llbuilder 
+		else _val
 
 and codegen_array_create llbuilder t el = 
     let revlist = List.rev el in 
@@ -353,7 +356,7 @@ and codegen_sexpr llbuilder = function
 	|   SAssign(e1, e2, d)        -> codegen_assign e1 e2 d llbuilder
 	|   SNoexpr d                 -> build_add (const_int i32_t 0) (const_int i32_t 0) "nop" llbuilder
 	|   SArrayCreate(t, el, d)    -> codegen_array_create llbuilder t el
-	|   SArrayAccess(e, el, d)    -> codegen_array_access e el d llbuilder
+	|   SArrayAccess(e, el, d)    -> codegen_array_access true e el d llbuilder
 	|   SObjAccess(e1, e2, d)     -> codegen_obj_access true e1 e2 d llbuilder
 	|   SCall(fname, el, d)       -> codegen_call llbuilder d el fname		
 	|   SObjectCreate(id, el, d)  -> codegen_obj_create id el d llbuilder
