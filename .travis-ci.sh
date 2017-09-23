a41fdb0d04927c4b4d10848bdd5fa96b8374dc62
David Watkins <djrival7@gmail.com>
2017-09-23 04:08:36 -0400

    Updated travis build file

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind llvm3.8"
+OPAM_DEPENDS="ocamlfind llvm=3.8"
 
 APT="ocaml ocaml-native-compilers camlp4-extra opam m4 clang-3.8 llvm opam ocaml"
-	 
-ppa=avsm/ppa
-	 
-echo "yes" | sudo add-apt-repository ppa:$ppa
+	 	 
+echo "yes" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq $APT
 export OPAMYES=1
