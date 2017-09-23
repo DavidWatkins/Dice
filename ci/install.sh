40181dedf8954673129bd2ba421d0d880203059c
David Watkins <djrival7@gmail.com>
2017-09-23 03:34:26 -0400

    Added proper ocaml build script

--- ci/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then exit; fi
-
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then 'ci/install_linux.sh' ; fi
--- ci/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$COMPILER" = "CLANG" ]; then exit; fi
-
-make all
